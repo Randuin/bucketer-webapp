fb94303ae9b9b55bd74644b2a35672ad8fd3964b
Robin Liao <randuin@gmail.com>
2011-12-09 20:04:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BucketerWebapp::Application.config.secret_token = '14ba354878fb82162ff6bd061b96194086cf6b6e6298762fb07ffe7efb877ead09bf9d9a624c564335359041d95e454f66ad7910fec6c10980a373e58953ccfc'
